a64cf23e87af23632d0089e33ded118dd0b5ecc1
Andrew <acater@gmail.com>
2017-01-15 01:17:21 -0500

    Changes to spawn

--- src/Spawn.java
+++ src/Spawn.java
@@ -21,15 +21,20 @@ public class Spawn
 
 		if(mobDelay == 0)
 		{
-		if (Game.gameState == Game.STATE.Game) 
-		{	
-				mobDelay = 30;
-//				zone = r.nextInt(10);
-//				if (zone == 0) 
-//				{
-					handler.addObject(new BasicEnemy(r.nextInt(400) + 400, 0, ID.BasicEnemy, handler));
-//				}
-//				handler.addObject(new BasicEnemy(680, 0, ID.BasicEnemy, handler));
+			if (Game.gameState == Game.STATE.Game) 
+			{	
+					mobDelay = 60;
+					zone = r.nextInt(4);
+					if (zone == 0) 
+					{
+						handler.addObject(new BasicEnemy(r.nextInt(500) + 400, 0, ID.BasicEnemy, handler,0,1));
+					}
+					else if(zone == 1)
+						handler.addObject(new BasicEnemy(1260, r.nextInt(500) + 200, ID.BasicEnemy, handler,-1,0));
+					else if(zone == 2)
+						handler.addObject(new BasicEnemy(r.nextInt(500) + 400, 960, ID.BasicEnemy, handler,0,-1));
+					else
+						handler.addObject(new BasicEnemy(0, r.nextInt(500) + 200, ID.BasicEnemy, handler,1,0));
 			}
 		}
 		else
